a0df75d9ad41a7bbfb98ac9f5344e9f345dc3d9d
franklinT <franklintsung3@gmail.com>
2022-07-31 15:31:29 -0400

    third

---
+++ server/schemas/resolver.js
@@ -0,0 +1,25 @@
+const { User, Book} = require('../models'); 
+const {AuthenticationError} = require('apollo-server-express'); 
+const {signToken} = require('../utils/auth'); 
+
+const resolvers = { 
+    Query: {
+        //get a user by user name 
+        me: async (parent, args, context)=> { 
+            if(context.user) { 
+                const userData = await User.findOne({})
+                .select('-__v -password')
+                .populate('books')
+                
+                return userData; 
+            }
+
+            throw new AuthenticationError('Not logged in!')
+        },
+    },
+
+    Mutation: { 
+        addUser: async ()
+    }
+
+}
